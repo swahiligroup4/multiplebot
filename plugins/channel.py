826e534535a96a58fba27481fdc408e92f13caf8
swahiligroup4 <53920453+swahiligroup4@users.noreply.github.com>
2023-05-04 12:39:11 +0300

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -1,5 +1,5 @@
 from info import filters
 from pyrogram  import Client
 @Client.on_message(filters.command('start') & filters.private)
-async def start_msg_admins(client, message)
+async def start_msg_admins(client, message):
     await message.reply_text('hi')
