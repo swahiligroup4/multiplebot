a7780f6853c93e5beda9661d8004dbe4ebfab3be
swahiligroup4 <53920453+swahiligroup4@users.noreply.github.com>
2023-05-04 12:19:44 +0300

    Update info.py

--- info.py
+++ info.py
@@ -9,7 +9,7 @@ id_pattern = re.compile(r'^.\d+$')
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = 10786281
 API_HASH = '5f42bc5562f6a1eb8bae8b77617186a0'
-BOT_TOKEN ='2138045217:AAEcyEaMnPiVUftD3y3-FQb-mk1ktc4t1Dw'
+BOT_TOKEN ='2136703772:AAH7YT8ngkmRmsSgU8BUX1zjQT8hw8JVdyE'
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
