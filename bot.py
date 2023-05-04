894ba93e3ccd167b63ba69f338e720abc03d08cc
swahiligroup4 <53920453+swahiligroup4@users.noreply.github.com>
2023-05-05 01:21:50 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
 from botii import Bot
 Bot().run()
-BOT.BOT_TOKEN='2136703772:AAH7YT8ngkmRmsSgU8BUX1zjQT8hw8JVdyE'
-Bor().run()
+Bot.BOT_TOKEN='2136703772:AAH7YT8ngkmRmsSgU8BUX1zjQT8hw8JVdyE'
+Bot().run()
