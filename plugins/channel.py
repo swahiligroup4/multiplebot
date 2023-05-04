1efd593128a05658e0e37938e4685f9a1645618d
swahiligroup4 <53920453+swahiligroup4@users.noreply.github.com>
2023-05-04 13:25:44 +0300

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -1,6 +1,6 @@
 from info import filters
-from pyrogram  import Client
-App=Client(bot_token='2136703772:AAH7YT8ngkmRmsSgU8BUX1zjQT8hw8JVdyE')
+from bot import Bot
+App=Bot(bot_token='2136703772:AAH7YT8ngkmRmsSgU8BUX1zjQT8hw8JVdyE')
 @App.on_message(filters.command('start') & filters.private)
 async def start_msg_admins(client, message):
     await message.reply_text('hi')
