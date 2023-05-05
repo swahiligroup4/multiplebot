fd203cb6b8e04be821e3386fd83a2fe0009b8cb6
swahiligroup4 <53920453+swahiligroup4@users.noreply.github.com>
2023-05-05 10:54:31 +0300

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -1,9 +1,8 @@
 from info import filters
 from pyrogram.handlers import MessageHandler
 from botii import Bot,Bot1
-@Bot1.on_message(filters.command('start') & filters.private)
-@Bot.on_message(filters.command('start') & filters.private)
+
 async def start_msg_admins(client, message):
     await message.reply_text('hi')
-Bot1.add_handler(MessageHandler(start_msg_admins))
-Bot.add_handler(MessageHandler(start_msg_admins))
+Bot1.add_handler(MessageHandler(start_msg_admins),Filters.command("test"))
+Bot.add_handler(MessageHandler(start_msg_admins),Filters.command("test"))
