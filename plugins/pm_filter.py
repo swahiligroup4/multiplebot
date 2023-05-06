1f937fe1d1afb08341481b2020494caaa0146462
swahiligroup4 <53920453+swahiligroup4@users.noreply.github.com>
2023-05-06 07:23:09 +0300

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -44,58 +44,37 @@ async def group(client, message):
                     reply_text = reply_text.replace("\\n", "\n").replace("\\t", "\t")
                 if acs == 'x':  
                     if fileid == 'None':
-                        await message.reply_text(text=f')
-                except:
-                    continue
-            elif msg_type == 'Photo' and file_status != 'normal':
-                try:
-                    result = InlineQueryResultPhoto(
-                        photo_url = fileid,
-                        title = keyword.upper(),
-                        description = descp,
+                        await message.reply_text(text=f'{reply_text}',reply_markup = reply_markup)
+               
+                elif msg_type == 'Photo' and file_status != 'normal':
+                    await message.reply_photo(
+                        photo_file_id = fileid,
                         caption = reply_text+'\nBonyeza **DOWNLOAD** kuipakua',
                         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('📤 Download', url=f"https://t.me/{nyva}?start=subinps_-_-_-_{id3}")]])if group_id != query.from_user.id else InlineKeyboardMarkup([[InlineKeyboardButton('📤 Download', url=f"https://t.me/{nyva}?start=subinps_-_-_-_{id3}")],[InlineKeyboardButton(' Edit', url=f"https://t.me/{nyva}?start=xsubinps_-_-_-_{id3}")]])
                     )
-                except:
-                    continue
-            elif msg_type == 'Photo':
-                try:
-                    result = InlineQueryResultPhoto(
-                        photo_url= fileid,
-                        title = keyword.upper(),
-                        description = descp,
+                
+                elif msg_type == 'Photo':
+                    await message.reply_photo(
+                        photo_file_id = fileid,
                         caption = reply_text or '',
                         reply_markup=reply_markup
                     )
-                except:
-                    continue
-            elif fileid and file_status != 'normal':
-                try:
-                    result = InlineQueryResultCachedDocument(
-                        document_file_id = fileid,
-                        title = keyword.upper(),
-                        description = descp,
+                
+                elif fileid and file_status != 'normal':
+                    await message.reply_cached_media(
+                        file_id = fileid,
                         caption = reply_text+'\nBonyeza **DOWNLOAD** kuipakua' or "",
-                        
                         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('📤 Download', url=f"https://t.me/{nyva}?start=subinps_-_-_-_{id3}")]])if group_id != query.from_user.id else InlineKeyboardMarkup([[InlineKeyboardButton('📤 Download', url=f"https://t.me/{nyva}?start=subinps_-_-_-_{id3}")],[InlineKeyboardButton(' Edit', url=f"https://t.me/{nyva}?start=xsubinps_-_-_-_{id3}")]])
                     )
-                except:
-                    continue
-            elif fileid:
-                try:
-                    result = InlineQueryResultCachedDocument(
-                        document_file_id = fileid,
-                        title = keyword.upper(),
-                        description = descp,
+                
+                elif fileid:
+                   await message.reply_cached_media(
+                        file_id = fileid,
                         caption = reply_text or "",
-                        
                         reply_markup=reply_markup
                     )
-                except:
-                    continue
-            else:
-                continue
-        if files:
+                
+        elif files:
             await message.reply_text(f"<b>Bonyeza kitufe <b>(🔍 Majibu ya Database : {len(files)})</b> Kisha chagua unachokipenda kwa kushusha chini\n\n💥Kwa urahisi zaidi kutafta chochote anza na aina kama ni  movie, series ,(audio ,video) kwa music , vichekesho kisha acha nafasi tuma jina la  kitu unachotaka mfano video jeje au audio jeje au movie extraction au series soz­</b>", reply_markup=get_reply_makup(searchi,len(files)))
         elif searchi.startswith('movie') or searchi.startswith('series') or searchi.startswith('dj'):
             await message.reply_text(text=f'Samahani **{searchi}** uliyotafta haipo kwenye database zetu.\n\nTafadhali bonyeza Button kisha ukurasa unaofuata ntumie jina la movie au series ntakupa jibu kwa haraka iwezekanavyo ili nii tafte',reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text='ADMIN',url=f'{user_id4}')]]))
