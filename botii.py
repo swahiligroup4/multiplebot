725d08bd939b080ece6b36015534e3d207511a52
swahiligroup4 <53920453+swahiligroup4@users.noreply.github.com>
2023-05-08 16:56:48 +0300

    Update botii.py

--- botii.py
+++ botii.py
@@ -49,6 +49,6 @@ class Bot1(Client):
         
     async def stop(self, *args):
         await super().stop()
-app=[Bot1()]       
+app=[Bot1(),Bot()]       
 async def main():
     await compose(app)
