7584809b3c722837c7220dd44d19b5ea7b0a2fd2
swahiligroup4 <53920453+swahiligroup4@users.noreply.github.com>
2023-05-08 16:30:24 +0300

    Update channel.py

--- plugin/channel.py
+++ plugin/channel.py
@@ -3,5 +3,5 @@ from botii import Bot1
 #@Bot.on_message(filters.command('start') & filters.private)
 @Bot1.on_message(filters.command('start') & filters.private)
 async def start_msg_admins(client, message):
-    await message.reply_text('hi')
-    #await message.reply_text(f'{mk.id}hi')
+    mk=await message.reply_text('hi')
+    await message.reply_text(f'{mk.id}hi')
