c12c9ca0241fb72fd5324060e028272181544428
swahiligroup4 <53920453+swahiligroup4@users.noreply.github.com>
2023-05-11 05:37:41 +0300

    Delete plugin directory

--- plugin/channel.py
+++
@@ -1,8 +0,0 @@
-from info import filters 
-from botii import Bot1,Bot
-@Bot.on_message(filters.command('start') & filters.private)
-@Bot1.on_message(filters.command('start') & filters.private)
-async def start_msg_admins(client, message):
-    mk = await client.send_message(text=f'hi',chat_id=message.from_user.id )
-    await message.reply_text(f'{mk.id}hi')
-    
