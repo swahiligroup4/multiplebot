8dee7ea7e22df8908f70541330448f7bc093b0f7
swahiligroup4 <53920453+swahiligroup4@users.noreply.github.com>
2023-05-15 23:09:37 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -43,6 +43,14 @@ class User(Document):
     email = fields.StrField(required=True)
     class Meta:
         collection_name = COLLECTION_NAME_2
+@instance2.register
+class Data(Document):
+    id = fields.IntField(attribute='_id')
+    group_id= fields.IntField(required=True)
+    status = fields.StrField(required=True)
+    email = fields.StrField(required=True)
+    class Meta:
+        collection_name = COLLECTION_NAME_2
 
 async def add_user(id, usr,sts):
     try:
