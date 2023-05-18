ed1e4e209dc16e07cbfc70bfc499ffc7d8d2f89a
swahiligroup4 <53920453+swahiligroup4@users.noreply.github.com>
2023-05-18 08:22:14 +0300

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -7,6 +7,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 async def start_msg_admins(client, message):
     mk=await message.reply_text('hi')
     mk2=await message.reply_text(f'{mk.id}hi',reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('yes', callback_data ='ccc')]]))
+    mk2 = await client.request_callback_answer(message.from_user.id,mk2.id,'ccc')
     mk=await message.reply_text(f'{mk2}')
 @Bot1.on_callback_query()
 async def about_ucbq(client, query):
