6a36bbbb6666dcb36eb4852b714d53a93a05f029
swahiligroup4 <53920453+swahiligroup4@users.noreply.github.com>
2023-05-18 08:07:22 +0300

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -3,7 +3,7 @@ from info import filters
 from botii import Bot,Bot1
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 @Bot.on_message(filters.command('start') & filters.private)
-@Bot1.on_message(filters.command('start') & filters.private)
+@Bot1.on_message(filters.command('test') & filters.private)
 async def start_msg_admins(client, message):
     mk=await message.reply_text('hi')
     await message.reply_text(f'{mk.id}hi',reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('yes',callbackquery ='ccc')]]))
