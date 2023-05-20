aca06d14efbe7149990c807efe63dfa10dfc759a
swahiligroup4 <53920453+swahiligroup4@users.noreply.github.com>
2023-05-20 08:40:45 +0300

    Update base_command.py

--- plugins/base_command.py
+++ plugins/base_command.py
@@ -1029,21 +1029,12 @@ def btn2(ab6,ab22):
     ab9=0
     for i in range(0,5):
         ab9=ab9+1
-        if ab7=="n":
-            ab8=f"season {ab9}"
-            ab11=ab9+1
-            ab10=f"season {ab11}"
-            ab.append([
-                InlineKeyboardButton(f"{ab8}", callback_data =f"sss s{ab8}"),
-                InlineKeyboardButton(f"{ab10}", callback_data =f"sss s{ab11}")
-            ])
-        elif ab6==10 or ab6==1:
-            ab8 = f"{ab6*(ab9-1)}1 hadi {ab6*(ab9)}0"
-            ab11 = ab9+1  
-            ab10 = f"{ab6*(ab9-1)}1 hadi {ab6*(ab9)}0"
+        if ab6==10 or ab6==1:
+            ab8 = f"{ab6*(ab9-1)}1 hadi {ab6*(ab9)}0" 
+            ab10 = f"{ab6*(ab9)}1 hadi {ab6*(ab9+1)}0"
             ab.append([
                 InlineKeyboardButton(f"{ab8}", callback_data =f"sss {ab22}.#{ab6*(ab9)}0"),
-                InlineKeyboardButton(f"{ab10}", callback_data =f"sss {ab22}.#{ab6*(ab11)}0")
+                InlineKeyboardButton(f"{ab10}", callback_data =f"sss {ab22}.#{ab6*(ab9+1)}0")
             ])
         ab9=ab9+1
     return InlineKeyboardMarkup(ab)
