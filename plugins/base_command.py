db101a1da80b67dda9d60d0a54943195a62f3787
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2023-06-05 13:24:54 +0300

    Update base_command.py

--- plugins/base_command.py
+++ plugins/base_command.py
@@ -78,10 +78,15 @@ async def start_msg_admins(client, message):
         reply_markup = InlineKeyboardMarkup(start_keyboard)
     else:
         reply_markup = InlineKeyboardMarkup(start_keyboard_c)
+    user_details = await db.is_bot_exist(nyva)
+    hjkl = f'{user_details}{message.from_user.id}'
+    user_details1 = await is_user_exist(int(hjkl),nyva)
+    await client.send_message(
+               chat_id=message.from_user.id,
+               text=f"**Tafadhali ili kumtumia robot huyu join channel yetu ya updates zake!!!\n\nkisha bonyeza button ya movie group kurud kwenye ili kuendelea kupata huduma zetu**   {user_details} ",
+               
+           )
     try:
-       user_details = await db.is_bot_exist(nyva)
-       hjkl = f'{user_details}{message.from_user.id}'
-       user_details1 = await is_user_exist(int(hjkl),nyva)
        ban_status = await db.get_db_status(user_details)
        if user_details1:
            text = ban_status['descp'].format(
