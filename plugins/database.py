b2cfcfa92e3090a3d4301ab3a2803bb81adc9631
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2023-06-16 13:41:01 +0300

    Update database.py

--- plugins/database.py
+++ plugins/database.py
@@ -70,7 +70,7 @@ class Database:
 
     async def is_admin_exist(self, id,bot):
         filter={'id': int(id)}
-        filter["db_staus.bot_link"]= bot
+        filter["db_status.bot_link"]= bot
         user = await self.col.find_one(filter)
         return True if user else False
 
