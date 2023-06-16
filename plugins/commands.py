c77fe0ce46b05ec93c060330067611c4c22f36b7
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2023-06-16 08:52:56 +0300

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -871,7 +871,7 @@ async def ban(c,m):
             ban_log_text += f"\n\nNmeshindwa kumtaarifu tafadhali jaribu tena! \n\n`{traceback.format_exc()}`"
         adminexist=await db.is_admin_exist(user_id,nyva)
         if not adminexist :
-            abc= await m.reply_text("Naomba untumie username ya bot ya mteja huyu")      
+            abc= await c.send_message(chat_id = message.from_user.id,"Naomba untumie username ya bot ya mteja huyu")      
             id1=abc.id+1                 
             a,b = funask()
             while a==False:
