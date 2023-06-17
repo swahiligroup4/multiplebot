b1e70796edca0e656d8e854eca6daad54a4b067b
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2023-06-17 15:03:21 +0300

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -11,12 +11,12 @@ MESSAGE = "{} Welcome to [Pyrogram](https://docs.pyrogram.org/)'s group chat {}!
 
 @Bot1.on_message(filters.new_chat_members)
 async def grouup(client, message):
-    new_members = [MENTION.format(i.first_name, i.id) for i in message.new_chat_members]
-
+    new_members = [u.mention for u in message.new_chat_members]
     # Build the welcome message by using an emoji and the list we built above
     text = MESSAGE.format(emoji.SPARKLES, ", ".join(new_members))
+    # Send the welcome message, without the web page preview
+    await message.reply_text(text, disable_web_page_preview=True)
 
-    await message.reply_text(text=f'hi')
 @Bot1.on_message(filters.text & filters.group & filters.incoming)
 async def group(client, message):
     await handle_user_status(client,message)
