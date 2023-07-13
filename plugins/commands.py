6ff602865f6a15dd529ad3c6febbc0dd8216d5cc
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2023-07-13 11:50:39 +0300

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -703,10 +703,10 @@ async def del_filter(client, message):
             quote=True
         )
         return
-    #text=f'{text1}.dd#.{message.from_user.id}'
+    text=f'{text1}.dd#.{message.from_user.id}'
     query = text1.lower()
     filter={'text': query}
-    details = await  get_filter_results(text,message.from_user.id)
+    details = await  get_filter_results(query,message.from_user.id)
     filter['group_id'] = message.from_user.id
     found =await Media.count_documents(filter)
     if int(found) >=1:
