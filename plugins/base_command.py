23437ae008456f906e57fbf42c053cecd0d1b319
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2023-07-22 16:42:16 +0300

    Update base_command.py

--- plugins/base_command.py
+++ plugins/base_command.py
@@ -389,7 +389,9 @@ async def cb_handler(client, query):
             await client.restrict_chat_member(int(query.data.split(" ")[1]), query.from_user.id,
                 ChatPermissions(can_send_messages=True,can_send_media_messages=True,can_send_other_messages=True)) 
             hjkl = f'{user_details}##{query.from_user.id}'
-            #await add_user(hjkl,nyva)
+            existt=await is_user_exist(hjkl,nyva)
+            if not existt:
+                await add_user(hjkl,nyva)
             inv_link=ban_status["group"].split("##")[1]
             await query.edit_message_text(text=f'✔️ Shukrani zetu zikufikie wewe uliweza kusoma mpaka hapa nahisi umetuelewa tunahusika na nini pia jinsi ya kupata huduma zetu..\n\n**Tumeshakuruhusu kutuma ujumbe kwenye kikundi ulichojiunga nacho** \n\nBonyeza **KIKUNDI** kurudi kwenye kikundi ',reply_markup=InlineKeyboardMarkup( [[InlineKeyboardButton("⬅️ BACK", callback_data =f'test1 {query.data.split(" ")[1]}'),InlineKeyboardButton("💥 KIKUNDI", url =f'{inv_link}') ]]))
         elif query.data.startswith("mbele"):
