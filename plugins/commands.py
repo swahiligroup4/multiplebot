ba1395b65f7c5a1999caeca98a4c826b69202e1b
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2023-07-22 15:42:57 +0300

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -908,11 +908,11 @@ async def ban(c,m):
                     a=False  
             strid = str(uuid.uuid4())
             ts=await c.get_users(user_id)
-            #await db.add_admin(user_id)
+            await db.add_admin(user_id)
             mk2=await db.get_db_status(user_id)
-            #await db.update_db(user_id,f'bot_link {mk.text.strip()}',mk2)
+            await db.update_db(user_id,f'bot_link {mk.text.strip()}',mk2)
             await db.update_db(user_id,f"user_link https://t.me/{ts.username}",mk2)
-            #await db.add_acc(strid,user_id,"all",user_id,9999)
+            await db.add_acc(strid,user_id,"all",user_id,9999)
         await db.ban_user(user_id, ban_duration)
         print(ban_log_text)
         await m.reply_text(
