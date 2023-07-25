4de1dcd1317ac2e38e92e6238130f62b00f512c1
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2023-07-25 14:57:11 +0300

    Update base_command.py

--- plugins/base_command.py
+++ plugins/base_command.py
@@ -520,6 +520,7 @@ async def cb_handler(client, query):
                     abdata = ""
                     ab1=ab1.split(" ")[1]
                     btn3=None
+                    await query.answer("🎙Subir kidogo",show_alert=True,cache_time=30)
                     ab2=int(ab2)
                     filez=await get_filter_results( ab3,user_details)
                     for file in reversed(filez): 
