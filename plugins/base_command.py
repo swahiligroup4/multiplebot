f0211c38aa56190947b55ac1b9fc20a6c5685331
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2023-07-26 17:36:58 +0300

    Update base_command.py

--- plugins/base_command.py
+++ plugins/base_command.py
@@ -523,6 +523,11 @@ async def cb_handler(client, query):
                             await asyncio.sleep(10)
                             if tme1 < 10:
                                 tme1=10
+                            user_dts=await is_user_exist(f"{user_details}##{query.from_user.id}",nyva)
+                            for usr1 in user_dts:
+                                tme2=usr1.tme
+                            if tme2==tme1:
+                                await User.collection.updateMany({},{'$set':{'tme':0}})
                             if (tme1-i-10) != 0:
                                 await abk.edit_text(text=f"tafadhali subir kwa sekunde {tme1-i-10} kabla ya kutuma ombi lingine")
                             else:
