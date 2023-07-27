53f96c5db5a848b1f5855f0c9498ba8da129b0aa
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2023-07-27 08:47:33 +0300

    Update base_command.py

--- plugins/base_command.py
+++ plugins/base_command.py
@@ -458,7 +458,8 @@ async def cb_handler(client, query):
                         dtc=s
                 if btn3==None:
                     rpymk=None
-                    await query.message.copy(chat_id=user_details,caption=f"{query.message.caption}\n\nSamahani kuna mteja alikuwa anaomba uweke season **{ab1}** ya series au movie hii..Kisha baada ya kuweka bonyeza done",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"__DONE__", callback_data =f"3hdns")]]))
+                    await query.message.copy(chat_id=user_details,caption=f"{query.message.caption}\n\nSamahani kuna mteja alikuwa anaomba uweke season **{ab1}** ya series au movie hii..Kisha baada ya kuweka bonyeza done ili tumtaarifu kuwa ushaiweka", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"✅  DONE__", callback_data =f"3hdns")]]))
+                    await query.message.edit_message_caption(caption=f"{query.messag.caption}\n\nSamahani mteja season uliokuwa unaiomba bado haijawekwa nmeshatoa taarifa kwa msimamizi wangu atakapoiweka tu ntakujuza..bonyeza rudi nyuma kutizama season nyingine",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"🔙 RUDI NYUMA", callback_data =f"3hvdo {ab2}")]]))
                 else:
                     for st in range(0,dtc,200):  
                         if st+200 <= dtc :
