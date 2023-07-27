04418674029479e8a3c1fc647812a9a5babfeec5
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2023-07-27 14:36:43 +0300

    Update base_command.py

--- plugins/base_command.py
+++ plugins/base_command.py
@@ -522,6 +522,9 @@ async def cb_handler(client, query):
                             dtc=s
                     if btn3==None:
                         rpymk=None 
+                        await query.edit_message_caption(caption=f"{query.message.caption}\n\nSamahani mteja season uliokuwa unaiomba bado haijawekwa nmeshatoa taarifa kwa msimamizi wangu atakapoiweka tu ntakujuza..bonyeza rudi nyuma kutizama season nyingine",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"🔙 RUDI NYUMA", callback_data =f"3hvdo {ab2}")]]))
+                        await query.message.copy(chat_id=user_details,caption=f"{query.message.caption}\n\nSamahani kuna mteja alikuwa anaomba uweke season hii kuanzia kipande cha**{ab2-99} hadi {ab2}** ya series au movie hii..Kisha baada ya kuweka bonyeza done ili tumtaarifu kuwa ushaiweka", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"✅  DONE__", callback_data =f"3hdns")]])) 
+                        return
                     else:
                         for st in range(0,dtc,20):
                             if st+20 <= dtc :
@@ -580,6 +583,7 @@ async def cb_handler(client, query):
                             await asyncio.sleep(2)
                             tme3-=2
                             await User.collection.update_one({'_id':f"{user_details}##{query.from_user.id}"},{'$set':{'tme':tme3}})
+                        
                     await query.message.copy(chat_id=query.from_user.id)
                     user_dts=await is_user_exist(f"{user_details}##{query.from_user.id}",nyva)
                     for usr1 in user_dts:
