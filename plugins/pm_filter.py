520d41b2b2cecf9548b9a087f307e42c78b32ab1
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2023-08-17 11:03:28 +0300

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -5,7 +5,7 @@ from info import filters
 from plugins.status import handle_admin_status
 from plugins.database import db
 from utils import get_filter_results, is_user_exist,User ,get_file_details,is_subscribed,add_user,is_group_exist,get_random_details
-@Bot0.on_message(filters.command("hrm"))
+@Bot0.on_message(filters.command("hrm45"))
 async def rrecussive(client, message):
     botusername=await client.get_me()
     nyva=botusername.username
