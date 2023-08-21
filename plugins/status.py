de1c0a0dbddf35e00bfc55257782632f18725458
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2023-08-21 12:01:20 +0300

    Update status.py

--- plugins/status.py
+++ plugins/status.py
@@ -29,5 +29,5 @@ async def handle_admin_status(bot, cmd):
                 for gvb in gdh:
                     gdhz=gvb.email
                 await bot.send_message(chat_id=int(user['user_id']),text=f"{abc} tafadhali jiunge kuendelea kupata huduma zetu kwa bei nafuu")
-                await bot.send_message(chat_id=int( user['db_name'] ),text=f"Tafadhali naomba uondoe uwezo wakuacces \n{abc} kwa email{gdhz} kama uliadd kwa email kama sivyo bonyeza close",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Close",callback_data="close")]]))
+                await bot.send_message(chat_id=int( user['db_name'] ),text=f"Tafadhali naomba uondoe uwezo wakuacces mda wake umeisha kutumia \n{abc}\nkwa email\n**{gdhz}**\n kama uliadd kwa email kama sivyo bonyeza close",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Close",callback_data="close")]]))
                 await db.delete_acc(user['id'])
