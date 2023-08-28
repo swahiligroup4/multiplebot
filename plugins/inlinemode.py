49c0c3134a84c77565784f345f00d90a472c4107
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2023-08-28 09:14:25 +0300

    Update inlinemode.py

--- plugins/inlinemode.py
+++ plugins/inlinemode.py
@@ -133,7 +133,7 @@ async def give_filter(client, query):
             title=title,
             input_message_content=InputTextMessageContent(message_text = f"Mpendwa [{query. from_user.first_name}](tg://user?id={query.from_user.id})\nKama movie yako haipo ntumie Mara moja jina lake kisha subir ntakujibu nkishaiadd kwenye database bonyeza kitufe hapo chini kutuma kisha ukurasa unaofuata bonyeza start kisha ntumie jina LA muv au series au nyimbo unayotafta", disable_web_page_preview = True),
             description=f'Hapa ndiyo mwisho wa  matokeo yetu kutoka kwenye database\nBonyeza hapa kama haipo kupata maelezo zaidi',
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Bonyeza hapa kutuma', url=f"{gd1}")]]))
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Bonyeza hapa kutuma', url=f"{gd1}")]])
         ))
     try:
         await query.answer(results=results,
