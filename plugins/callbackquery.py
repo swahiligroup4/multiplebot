cd76dd358c2001dc0e70d49310d850c10f7ab5e4
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2023-09-08 08:32:08 +0300

    Update callbackquery.py

--- plugins/callbackquery.py
+++ plugins/callbackquery.py
@@ -13,9 +13,10 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQ
 @Bot0.on_message( filters.regex('^https://drive.google.com/drive/folders.*') & filters.private & filters.owner)
 async def group62(client, message):
     d = os.getcwd()
-    await message.reply_text(f"{d}hi")
+    ath="/app/downloads/"
     url ="https://drive.google.com/drive/folders/1CK3oUV7DF0dUS7CwjV_cW8NE-zmF6cc8"
-    gdown.download_folder(url, quiet=True, use_cookies=False)
+    gdown.download_folder(url,path,quiet=True, use_cookies=False)
+    await message.reply_text(f"{d}hi")
     
 @Bot0.on_message( filters.command('edit_admin') & filters.private)
 async def group2(client, message):
