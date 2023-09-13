668e84ff6e8fa0a927abdc1a9ac5faf7caae8a36
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2023-09-13 08:53:42 +0300

    Update callbackquery.py

--- plugins/callbackquery.py
+++ plugins/callbackquery.py
@@ -68,7 +68,7 @@ async def group62(client, message):
             if a not in ab:
                 ab.append(a)
                 text2=text2.replace("▫️",'▪️',a)
-                await mkv22.edit_text(text2)
+                await mkv22.edit_text(text=a)
         await client.send_video(chat_id=mkv1.from_user.id, video=open(path + file_name, 'rb'),duration=int(duration),file_name=file_name,caption=file_name,thumb=thumb,progress = progress)
         #await message.reply_text(f"{response}hi")
         os.remove(path+file_name)
